d4be8193202531d615d5562edf39e67540187cc8
William Story <william.story.e@gmail.com>
2025-04-08 14:36:43 -0600

    Add html node class and tests to cover props_to_html()

---
+++ src/htmlnode.py
@@ -0,0 +1,18 @@
+class HTMLNode:
+    def __init__(self=None, tag=None, value=None, children=None, props=None):
+        self.tag = tag
+        self.value = value
+        self.children = children
+        self.props = props
+    
+    def to_html(self):
+        raise NotImplementedError
+
+    def props_to_html(self):
+        html_string = ""
+        for key, value in self.props.items():
+            html_string += f"{key}=\"{value}\" "
+        return html_string.strip()
+            
+    def __repr__(self):
+        return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
---
+++ src/test_htmlnode.py
@@ -0,0 +1,16 @@
+import unittest
+
+from htmlnode import HTMLNode
+
+class TestHTMLNode(unittest.TestCase):
+    def test_props_to_html(self):
+        node = HTMLNode("div", "value", None, {"class": "container", "id": "main"})
+        self.assertEqual(node.props_to_html(), "class=\"container\" id=\"main\"")
+        
+    def test_props_to_html_none(self):
+        node = HTMLNode("div", "value", None, {})
+        self.assertEqual(node.props_to_html(), "")
+    
+    def test_props_to_html_single(self):
+        node = HTMLNode("div", "value", None, {"class": "container"})
+        self.assertEqual(node.props_to_html(), "class=\"container\"")
